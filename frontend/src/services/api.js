319d88492f1557385f51c331a72fb29959bc0172
yueyao926 <3250102601@zju.edu.cn>
2026-06-14 23:06:21 +0800

    chore: prepare frontend api proxy for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-})
+  baseURL: "/api",
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token")
